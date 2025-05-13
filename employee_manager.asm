6caa9e5d9f47807567fa5784357d6d679e44ae00
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2025-05-13 23:23:36 +0800

    Updated code

--- employee_manager.asm
+++ employee_manager.asm
@@ -25,19 +25,23 @@ Employee ENDS
 
     employee1 Employee <>
 
-    promptMessage      db "Enter a number (1-4): ", 0
+    promptMessage      db "Enter a number (1-4): ", 10,13,0
     inputBuffer        db 16 dup(0)
-    createMsg          db "Create a new employee profile", 13, 10, 0
-    addMsg             db "Add new employee profile", 13, 10, 0
-    exitMsg            db "Exit", 13, 10, 0
-    invalidMsg         db "Invalid input!", 13, 10, 0
+    createMsg          db "Create a new employee profile", 10, 13, 0
+    addMsg             db "Add new employee profile", 10, 13, 0
+    exitMsg            db "Exit", 10, 13, 0
+    invalidMsg         db "Invalid input!", 10, 13, 0
+
+    searchMsg db "Enter employee ID:", 10, 13, 0
+    empFoundMsg db "Employee found!", 10, 13, 0
+    empNotFoundMsg db "Employee not found", 10, 13, 0
 
     ; Employee prompts
-    empIdPrompt       db "Enter Employee ID: ",0
-    empNamePrompt     db "Enter Employee Name: ",0
-    empPositionPrompt db "Enter Position: ",0
-    empSalaryPrompt   db "Enter Salary: ",0
-    empDatePrompt     db "Enter Date Hired: ",0
+    empIdPrompt       db "Enter Employee ID: ",10,13,0
+    empNamePrompt     db "Enter Employee Name: ",10,13,0
+    empPositionPrompt db "Enter Position: ",10,13,0
+    empSalaryPrompt   db "Enter Salary: ",10,13,0
+    empDatePrompt     db "Enter Date Hired: ",10,13,0
 
     ; Employee data buffers
     empID             db 32 dup(0)
@@ -48,23 +52,23 @@ Employee ENDS
 
 .code
 start:
-    ; Prompt the user
+
+MainMenu:
     invoke StdOut, addr promptMessage
-
-    ; Get input from console
+    invoke StdOut, addr createMsg
+    invoke StdOut, addr addMsg
+    invoke StdOut, addr exitMsg
     invoke StdIn, addr inputBuffer, sizeof inputBuffer
-
-    ; Convert input to integer
     invoke atodw, addr inputBuffer
-    mov eax, eax  ; result is in EAX
+    mov eax, eax  ; EAX = choice
 
-    ; Check and compare input
     cmp eax, 1
     je CreateProfile
     cmp eax, 2
-    je AddProfile
+    je SearchProfile
     cmp eax, 3
     je ExitProgram
+    jmp InvalidInput
 
 InvalidInput:
     invoke StdOut, addr invalidMsg
@@ -104,15 +108,85 @@ CreateProfile:
     invoke StdOut, addr empDatePrompt
     invoke StdIn, addr employee1.empDateHired, sizeof employee1.empDateHired
 
-    jmp Done
+    jmp MainMenu
 
 ArrayFull:
     invoke StdOut, addr invalidMsg ; Or a "list full" message
     jmp Done
 
-AddProfile:
-    invoke StdOut, addr addMsg
-    jmp Done
+SearchProfile:
+    ; Ask for employee ID
+    invoke StdOut, addr searchMsg
+    invoke StdOut, addr empIdPrompt
+    invoke StdIn, addr inputBuffer, sizeof inputBuffer
+    invoke atodw, addr inputBuffer  ; Convert input to number
+    mov ebx, eax  ; EBX = employee ID to search for
+
+    ; Start searching for the employee in the array
+    lea esi, employeeArray
+    mov ecx, employeeCount  ; How many employees to search through
+
+    ; Search loop
+    xor eax, eax  ; EAX = index
+SearchLoop:
+    cmp eax, ecx
+    jge EmployeeNotFound  ; If we've searched through all employees, exit
+
+    ;Calculate the address of the current employee
+    lea edi, [employeeArray]     ; EDI points to the start of the employee array
+    mov ebx, eax                 ; EBX = index (employee number)
+    mov ecx, SIZEOF Employee     ; Load the size of Employee structure into ECX
+    mul ecx                      ; Multiply index by the size of the Employee structure (result in EAX)
+    add edi, eax                 ; EDI now points to the correct employee (employeeArray[index])
+
+    ;Access empID field using correct offset
+    lea edx, [edi + offset Employee.empID]   ; EDx = address of empID in the current employee
+    mov esi, [edx]                           ; Copy empID to ESI for comparison
+
+    ; Compare IDs
+    cmp esi, ebx
+    je EmployeeFound
+
+    inc eax  ; Move to next employee
+    jmp SearchLoop
+
+EmployeeFound:
+    ; Display the employee details
+    invoke StdOut, addr empFoundMsg
+
+    ;invoke StdOut, addr edi.empID
+    lea eax, [edi]
+    add eax, offset Employee.empID
+    invoke StdOut, eax
+
+    ;invoke StdOut, addr edi.empName
+    lea eax, [edi]
+    add eax, offset Employee.empName
+    invoke StdOut, eax
+    
+    ;invoke StdOut, addr edi.empPosition
+    lea eax, [edi]
+    add eax, offset Employee.empPosition
+    invoke StdOut, eax
+
+    ;invoke StdOut, addr edi.empSalary
+    lea eax, [edi]
+    add eax, offset Employee.empSalary
+    invoke StdOut, eax
+
+    ;invoke StdOut, addr edi.empDateHired
+    lea eax, [edi]
+    add eax, offset Employee.empDateHired
+    invoke StdOut, eax
+
+    ; Go back to the main menu
+    jmp MainMenu
+
+EmployeeNotFound:
+    ; No employee found
+    invoke StdOut, addr empNotFoundMsg
+    jmp MainMenu
+
 
 ExitProgram:
     invoke StdOut, addr exitMsg
