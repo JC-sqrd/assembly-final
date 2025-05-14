cc40f31ba9b2837d5a884322d0c8b57d025fc685
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2025-05-14 23:24:53 +0800

    Fixed search logic and added deletion of existing employee profiles

--- employee_manager.asm
+++ employee_manager.asm
@@ -27,11 +27,13 @@ Employee ENDS
 
     promptMessage      db "Enter a number (1-4): ", 10,13,0
     inputBuffer        db 16 dup(0)
-    createMsg          db "Create a new employee profile", 10, 13, 0
-    addMsg             db "Add new employee profile", 10, 13, 0
-    exitMsg            db "Exit", 10, 13, 0
+    createMsg          db "1. Create a new employee profile", 10, 13, 0
+    addMsg             db "2. Search employee profile", 10, 13, 0
+    deleteMsg          db "3. Delete existing employee profile", 10, 13, 0
+    exitMsg            db "4. Exit", 10, 13, 0
     invalidMsg         db "Invalid input!", 10, 13, 0
-
+    empDeletedMsg      db "Employee successfully deleted", 10, 13, 0
+    
     searchMsg db "Enter employee ID:", 10, 13, 0
     empFoundMsg db "Employee found!", 10, 13, 0
     empNotFoundMsg db "Employee not found", 10, 13, 0
@@ -42,6 +44,7 @@ Employee ENDS
     empPositionPrompt db "Enter Position: ",10,13,0
     empSalaryPrompt   db "Enter Salary: ",10,13,0
     empDatePrompt     db "Enter Date Hired: ",10,13,0
+    newLine db 10,13,0
 
     ; Employee data buffers
     empID             db 32 dup(0)
@@ -57,6 +60,7 @@ MainMenu:
     invoke StdOut, addr promptMessage
     invoke StdOut, addr createMsg
     invoke StdOut, addr addMsg
+    invoke StdOut, addr deleteMsg
     invoke StdOut, addr exitMsg
     invoke StdIn, addr inputBuffer, sizeof inputBuffer
     invoke atodw, addr inputBuffer
@@ -67,6 +71,8 @@ MainMenu:
     cmp eax, 2
     je SearchProfile
     cmp eax, 3
+    je DeleteProfile
+    cmp eax, 4
     je ExitProgram
     jmp InvalidInput
 
@@ -75,6 +81,8 @@ InvalidInput:
     jmp Done
 
 CreateProfile:
+
+    invoke ClearScreen
     ; Check if we have space
     mov eax, employeeCount
     cmp eax, MAX_EMPLOYEES
@@ -108,6 +116,21 @@ CreateProfile:
     invoke StdOut, addr empDatePrompt
     invoke StdIn, addr employee1.empDateHired, sizeof employee1.empDateHired
 
+    ; Copy employee1 into employeeArray[employeeCount]
+    mov ecx, SIZEOF Employee
+    lea esi, employee1
+
+    mov eax, employeeCount
+    mov edx, SIZEOF Employee
+    mul edx                      ; EAX = employeeCount * SIZEOF Employee
+    lea edi, employeeArray
+    add edi, eax                 ; EDI = &employeeArray[employeeCount]
+
+    mov ecx, SIZEOF Employee
+    rep movsb
+
+    inc employeeCount
+    
     jmp MainMenu
 
 ArrayFull:
@@ -115,6 +138,7 @@ ArrayFull:
     jmp Done
 
 SearchProfile:
+    invoke ClearScreen
     ; Ask for employee ID
     invoke StdOut, addr searchMsg
     invoke StdOut, addr empIdPrompt
@@ -140,11 +164,9 @@ SearchLoop:
     add edi, eax                 ; EDI now points to the correct employee (employeeArray[index])
 
     ;Access empID field using correct offset
-    lea edx, [edi + offset Employee.empID]   ; EDx = address of empID in the current employee
-    mov esi, [edx]                           ; Copy empID to ESI for comparison
-
-    ; Compare IDs
-    cmp esi, ebx
+    lea eax, [edi + offset Employee.empID]   ; eax = address of stored empID
+    invoke lstrcmp, eax, addr inputBuffer    ; Compare it to user input
+    cmp eax, 0
     je EmployeeFound
 
     inc eax  ; Move to next employee
@@ -158,26 +180,31 @@ EmployeeFound:
     lea eax, [edi]
     add eax, offset Employee.empID
     invoke StdOut, eax
+    invoke StdOut, addr newLine
 
     ;invoke StdOut, addr edi.empName
     lea eax, [edi]
     add eax, offset Employee.empName
     invoke StdOut, eax
+    invoke StdOut, addr newLine
     
     ;invoke StdOut, addr edi.empPosition
     lea eax, [edi]
     add eax, offset Employee.empPosition
     invoke StdOut, eax
+    invoke StdOut, addr newLine
 
     ;invoke StdOut, addr edi.empSalary
     lea eax, [edi]
     add eax, offset Employee.empSalary
     invoke StdOut, eax
+    invoke StdOut, addr newLine
 
     ;invoke StdOut, addr edi.empDateHired
     lea eax, [edi]
     add eax, offset Employee.empDateHired
     invoke StdOut, eax
+    invoke StdOut, addr newLine
 
     ; Go back to the main menu
     jmp MainMenu
@@ -187,7 +214,80 @@ EmployeeNotFound:
     invoke StdOut, addr empNotFoundMsg
     jmp MainMenu
 
+DeleteProfile:
+    ; Ask for employee ID to delete
+    invoke StdOut, addr deleteMsg
+    invoke StdOut, addr empIdPrompt
+    invoke StdIn, addr inputBuffer, sizeof inputBuffer
 
+    ; Start searching for the employee
+    lea esi, employeeArray
+    mov ecx, employeeCount
+
+    xor eax, eax   ; Index
+    
+SearchDeleteLoop:
+    cmp eax, ecx
+    jge EmployeeNotFound
+
+    mov edx, eax
+    mov ebx, SIZEOF Employee
+    mul ebx
+    lea edi, employeeArray
+    add edi, eax  ; edi = &employeeArray[index]
+
+    ; Compare empID
+    lea eax, [edi + offset Employee.empID]
+    invoke lstrcmp, eax, addr inputBuffer
+    cmp eax, 0
+    je DeleteFound
+
+    inc edx
+    mov eax, edx
+    jmp SearchDeleteLoop
+
+DeleteFound:
+    ; edx = index of employee to delete
+    ; Shift remaining entries left
+    mov esi, edx
+    inc esi             ; esi = index of next employee (i+1)
+    mov ecx, employeeCount
+    sub ecx, esi        ; ecx = how many entries to move
+
+    cmp ecx, 0
+    jle SkipShift       ; No entries to move if deleting last one
+
+    mov ebx, SIZEOF Employee
+
+ShiftLoop:
+    mov eax, esi
+    mul ebx
+    lea esi, employeeArray
+    add esi, eax     ; esiPtr = &employeeArray[i+1]
+
+    mov eax, edx
+    mul ebx
+    lea edi, employeeArray
+    add edi, eax     ; ediPtr = &employeeArray[i]
+
+    mov ecx, SIZEOF Employee
+    ;mov esi, esiPtr
+    ;mov edi, ediPtr
+    rep movsb
+
+    inc edx
+    inc esi
+    dec ecx
+    jmp ShiftLoop
+    ;loop ShiftLoop
+
+SkipShift:
+    ; Decrement employee count
+    dec employeeCount
+
+    invoke StdOut, addr empDeletedMsg
+    jmp MainMenu
+    
 ExitProgram:
     invoke StdOut, addr exitMsg
     jmp Done
